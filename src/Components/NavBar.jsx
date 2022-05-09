18ff1d5f8a5e55563a1923a1b96e39e93c3c52ed
Bruno <bruno_links@hotmail.com>
2022-05-08 21:31:21 -0300

    second commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import "../App.css";
 
 function Navbar() {
-
-    return <div className="NavBar"> </div>
+  return (
+    <div className="Navbar">
+      <div className="leftside">
+        <div className="links">
+          <a href="/home">Home</a>
+          <a href="/feedback">Feedback</a>
+          <a href="/aboutus">About Us</a>
+          <a href="/contactus">Contact Us</a>
+        </div>
+      </div>
+      <div className="rightside"></div>
+    </div>
+  );
 }
+
+export default Navbar;
